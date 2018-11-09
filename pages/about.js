5347aee4348257ca67bd47bf4e690502d63aafaa
Blake Bishop <blake.bishop@kuali.co>
2018-11-08 18:26:30 -0700

    Try to use packages

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import { Component } from "react";
-import Link from "next/link";
-import Header from "../components/header";
+import { Component } from 'react'
+import Link from 'next/link'
+import Header from '../components/header'
 
 class AboutPage extends Component {
   static getInitialProps() {
-    const isServer = typeof window === "undefined";
-    return { isServer };
+    const isServer = typeof window === 'undefined'
+    return { isServer }
   }
 
   render() {
@@ -15,18 +15,18 @@ class AboutPage extends Component {
         <section>
           <p>
             This is another page of the SSR example, you accessed it{" "}
-            <strong>{this.props.isServer ? "server" : "client"} side</strong>.
+            <strong>{this.props.isServer ? 'server' : 'client'} side</strong>.
           </p>
           <p>
             You can reload to see how the page change.
           </p>
-          <Link href="/">
+          <Link href='/'>
             <a>Go to Home</a>
           </Link>
         </section>
       </main>
-    );
+    )
   }
 }
 
-export default AboutPage;
+export default AboutPage
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Link from "next/link";
-import Header from "../components/header";
+import Link from 'next/link'
+import Header from '../components/header'
+import cowsay from 'cowsay-browser'
 
 export default () => (
   <main>
     <Header />
+    <pre>
+      { cowsay.say({ text: 'Yehaw cowboy' }) }
+    </pre>
     <section>
       <Link href="/about">
         <a>Go to About Me</a>
       </Link>
     </section>
   </main>
-);
+)
